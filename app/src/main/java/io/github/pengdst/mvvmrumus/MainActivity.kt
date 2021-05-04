01cf2ccd98411b0b456f2c8c5f405f994224ea5d
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-04 14:10:13 +0700

    setup viewbinding dan viewmodel

--- app/src/main/java/io/github/pengdst/mvvmrumus/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/mvvmrumus/MainActivity.kt
@@ -3,15 +3,33 @@ package io.github.pengdst.mvvmrumus
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import io.github.pengdst.mvvmrumus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        initViewModel()
+        subscribeViewModel()
+    }
+
+    private fun subscribeViewModel() {
+        viewModel.observeLuasPersegiPanjang().observe(this) { luas ->
+
+        }
+
+        viewModel.observeKelilingPersegiPanjang().observe(this) { keliling ->
+
+        }
+    }
+
+    private fun initViewModel() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
