497293479e8aff88c0ecb506b604007894b9738b
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-04 12:21:22 +0700

    First commit

---
+++ app/src/main/java/io/github/pengdst/mvvmrumus/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.github.pengdst.mvvmrumus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
